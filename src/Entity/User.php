6daf54e09e206e81d77f9f4996dcfb173243d99f
Kouyoumjian <adrien.kouyoumjian@outlook.fr>
2023-07-25 15:19:51 +0200

    push

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,10 +2,13 @@
 
 namespace App\Entity;
 
+use DateTime;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
@@ -19,31 +22,86 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["user:read"])] 
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: "L'email ne peut pas être vide.")]
+    #[Assert\Email(message: "L'email n'est pas valide.")]
+    #[Assert\Length(
+        min: 5,
+        max: 70,
+        minMessage: "L'email doit comporter au moins {{ limit }} caractères.",
+        maxMessage: "L'email ne peut pas dépasser {{ limit }} caractères."
+    )]
+    #[Groups(['user:read', 'user:create', "user:update"])]
     private ?string $email = null;
 
     #[ORM\Column]
+    #[Groups(["user:read"])] 
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Groups("user:create", "user:update")]
+    #[Assert\NotBlank(message: "Le mot de passe ne peut pas être vide.")]
+    #[Assert\Length(
+        min: 6,
+        max: 100,
+        minMessage: "Le mot de passe doit comporter au moins {{ limit }} caractères.",
+        maxMessage: "Le mot de passe ne peut pas dépasser {{ limit }} caractères."
+    )]
+    #[Assert\Regex(
+        pattern:"/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.,])[A-Za-z\d@$!%*?&.,]{8,}$/",
+        message:"Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial (@, $, !, %, *, ?, &, .)."
+    )]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le nom ne peut pas être vide.")]
+    #[Assert\Length(
+        min: 2,
+        max: 40,
+        minMessage: "Le nom doit comporter au moins {{ limit }} caractères.",
+        maxMessage: "Le nom ne peut pas dépasser {{ limit }} caractères."
+    )]
+    #[Groups(['user:read', 'user:create'])]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(["user:read", "user:create"])]
+    #[Assert\NotBlank(message: "Le prénom ne peut pas être vide.")]
+    #[Assert\Length(
+        min: 2,
+        max: 40,
+        minMessage: "Le prénom doit comporter au moins {{ limit }} caractères.",
+        maxMessage: "Le prénom ne peut pas dépasser {{ limit }} caractères."
+    )]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le numéro de téléphone ne peut pas être vide.")]
+    #[Assert\Length(
+        max: 20,
+        maxMessage: "Le numéro de téléphone ne peut pas dépasser {{ limit }} caractères."
+    )]
+    #[Assert\Regex(
+        pattern:"/^\+?[0-9]+$/",
+        message:"Le numéro de téléphone doit contenir seulement des chiffres et un signe '+'."
+    )]
+    #[Groups(["user:read", "user:create", "user:update"])]
     private ?string $phoneNumber = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups("user:read")] 
     private ?\DateTimeInterface $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+    }
 
     public function getId(): ?int
     {
