9b79f3176a28c5465f1a48a5a8df91ed2954284a
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-09-21 18:11:07 +0200

    x

--- src/Controller/Admin/CompanyCrudController.php
+++ src/Controller/Admin/CompanyCrudController.php
@@ -9,7 +9,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
--- src/Controller/Admin/InvoiceCrudController.php
+++ src/Controller/Admin/InvoiceCrudController.php
@@ -6,7 +6,6 @@ use App\Entity\Invoice;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
