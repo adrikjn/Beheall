fbe2d731b4d1525fc322af5cc241b8dbb0e84c2d
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-09-21 12:35:00 +0200

    x

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -32,7 +33,7 @@ class UserCrudController extends AbstractCrudController
             EmailField::new('email')->onlyOnIndex(),
             EmailField::new('email')->onlyWhenCreating(),
             TextField::new('password', 'Mot de passe')->setFormType(PasswordType::class)->onlyWhenCreating(),
-            
+
             TextField::new('lastName', 'Nom')->onlyOnIndex(),
             TextField::new('lastName', 'Nom')->onlyWhenCreating(),
             TextField::new('firstName', 'Prénom')->onlyOnIndex(),
@@ -40,6 +41,21 @@ class UserCrudController extends AbstractCrudController
             TextField::new('phoneNumber', 'Numéro de téléphone')->onlyOnIndex(),
             TextField::new('phoneNumber', 'Numéro de téléphone')->onlyWhenCreating(),
             DateTimeField::new('createdAt', "Créer le")->setFormat('d/M/Y à H:m:s')->hideOnForm(),
+            CollectionField::new('roles')->setTemplatePath('admin/field/roles.html.twig'),
+            CollectionField::new('companies', 'Entreprises')
+                ->onlyOnIndex()
+                ->formatValue(function ($value, $entity) {
+                    // Créez un tableau pour stocker les IDs des entreprises
+                    $companyIds = [];
+
+                    // Parcourez les entreprises associées à l'utilisateur
+                    foreach ($entity->getCompanies() as $company) {
+                        $companyIds[] = $company->getId();
+                    }
+
+                    // Retournez les IDs sous forme de chaîne de caractères séparée par des virgules
+                    return implode(', ', $companyIds);
+                }),
         ];
     }
 
