c9809833d16035c35242d40eeb8ac08b961d87c5
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-09-25 19:01:50 +0200

    x

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Middleware;
@@ -7,13 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    public function __invoke(Request $request, Response $response)
+    public function __invoke(Request $request, callable $next)
     {
+        // Exécutez la prochaine étape du middleware, généralement le contrôleur
+        $response = $next($request);
+
+        // Configurez les en-têtes CORS
         $response->headers->set('Access-Control-Allow-Origin', 'https://www.beheall.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        // Allow credentials if needed
+        // Permettre les cookies si nécessaire
         // $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         return $response;
