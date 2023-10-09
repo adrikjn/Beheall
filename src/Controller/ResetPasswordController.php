f41ac212cd76611a7adc858f21b94c5a358b372b
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2023-10-09 15:37:03 +0200

    x

---
+++ src/Controller/ResetPasswordController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ResetPasswordController extends AbstractController
+{
+    /**
+ * @Route("/reset-password/confirm/{token}", name="reset_password_confirm")
+ */
+public function confirmResetPassword(string $token): Response
+{
+    // Vérifiez le jeton et effectuez la réinitialisation du mot de passe ici.
+    // Vous pouvez également afficher un formulaire de réinitialisation de mot de passe.
+
+    return $this->render('reset_password/confirm.html.twig', [
+        'token' => $token,
+    ]);
+}
+    /**
+     * @Route("/reset-password", name="reset_password", methods={"POST"})
+     */
+    public function resetPassword(Request $request, MailerInterface $mailer): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'];
+
+        // Générez un jeton unique (par exemple, une chaîne aléatoire)
+        $token = bin2hex(random_bytes(32));
+
+        // Envoyez un e-mail à l'utilisateur avec le lien de réinitialisation
+        $resetUrl = $this->generateUrl('reset_password_confirm', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $email = (new Email())
+            ->from('beheallpro@outlook.com')
+            ->to($email)
+            ->subject('Réinitialisation de mot de passe')
+            ->html(
+                $this->renderView(
+                    'reset_password/index.html.twig',
+                    ['resetUrl' => $resetUrl]
+                )
+            );
+
+        $mailer->send($email);
+
+        return new Response('E-mail de réinitialisation envoyé avec succès !');
+    }
+}
